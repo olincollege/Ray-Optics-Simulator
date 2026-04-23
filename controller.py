8dda3c221b061ad2e21bd7296e6aa1a8c39738ef
HaydenZS <hzangerlsalter@olin.edu>
2026-04-23 21:57:36 +0000

    explaining controller and starting model tests

--- controller.py
+++ controller.py
@@ -33,15 +33,19 @@ class CommandLine():
         Main loop for grabbing and using user inputs
         """
         while self.quit_var==0:
-            _cmd = input('Enter a command: ').split(' ')
+            _cmd = input('Enter a command: ').split(' ') 
+            # Clean up user input, unstring floats
             for i,element in enumerate(_cmd):
                 try:
                     _cmd[i]=float(element)
                 except ValueError:
                     pass
+
+            # ['model', 'create', 'lens', 1, 0, .125, .25, 2]
             if len(_cmd)>1:
                 if _cmd[1]=='create':
                     _cmd.append(self.model_instance)
+            # ['model', 'create', 'lens', 1, 0, .125, .25, 2, model]
             try:
                 self.commands[_cmd[0]]()
             except KeyError:
@@ -49,16 +53,23 @@ class CommandLine():
             except TypeError:
                 try:
                     self.commands[_cmd[0]](*_cmd[1:])
+                    # self.commands['model'](['create', 'lens', 1, 0, .125, .25, 2, model])
+                    # model.function_dict(['create', 'lens', 1, 0, .125, .25, 2, model])
                 except KeyError:
                     print('Invalid Command!')
                 except TypeError:
                     try:
                         self.commands[_cmd[0]][_cmd[1]](*_cmd[2:])
+                        # model.function_dict['create'](['lens', 1, 0, .125, .25, 2, model])
+                        # {'source':LightSource,'lens':IdealLens}(['lens', 1, 0, .125, .25, 2, model])
                     except KeyError:
                         print('Invalid Command!')
                     except TypeError:
                         try:
                             self.commands[_cmd[0]][_cmd[1]][_cmd[2]](*_cmd[3:])
+                        # IdealLens([1, 0, .125, .25, 2, model])
+                        # This is the call of IdealLens with the provided parameters
+
                         except KeyError:
                             print('Invalid Command!')
                         except TypeError:
--- test_model.py
+++ test_model.py
@@ -4,6 +4,7 @@ from model import Model, IdealLens, LightSource, LightRay
 
 def test_new_source():
     test_model = Model()
-    test_source = LightSource()
-    tester.new_source(test_model,)
-    assert ASSERTION_HERE
+    test_source = LightSource('standard',0,0,0,0,test_model)
+    print('test')
+    print(test_model)
+    assert test_model._source==test_source
